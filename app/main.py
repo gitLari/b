af992c8a29a7954ff8447f5b4916bb3199cd5776
GLEB TISHCHENKO <Gleb@MacGook.local>
2019-09-15 22:51:01 +0300

    lisää javascript perustaikaa

--- app/main.py
+++ app/main.py
@@ -5,13 +5,15 @@ import random
 
 app = Flask(__name__)
 
+kurssit = ["Docker", "Jenkins", "Vagrant"]
+verkkokurssit = ["GIT", "Linux", "RESTful API"]
+tyoviikko = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
+kuukauded = ["Tammikuulle", "Helmikuulle", "Maalliskuulle", "Huhtikuulle", "Toukokuulle", "Kesäkuulle", "Heinäkuulle", "Elokuulle", "Syyskuulle", "Marraskuulle", "Joulukuulle"]
+tanaan = date.today()
+poissadata = dict()
+
 @app.route('/oppija/<name>', methods=['GET'])
 def app_name(name):
-    kurssit = ["Docker", "Jenkins", "Vagrant"]
-    verkkokurssit = ["GIT", "Linux", "RESTful API"]
-    tyoviikko = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
-    kuukauded = ["Tammikuulle", "Helmikuulle", "Maalliskuulle", "Huhtikuulle", "Toukokuulle", "Kesäkuulle", "Heinäkuulle", "Elokuulle", "Syyskuulle", "Marraskuulle", "Joulukuulle"]
-    tanaan = date.today()
     vuo = int(tanaan.strftime("%Y"))
     kuu = tanaan.strftime("%m")
 
@@ -31,10 +33,8 @@ def app_name(name):
                 sattu = random.randrange(len(kurssit))
                 aihe = kurssit[sattu]
                 if i <= 4:
-                    lukuhtml = lukuhtml + "<tr><td>" + str(paiva) + "</td><td>" + tyoviikko[i] + "</td><td>" + aihe + "</td><td><button type = \"button\" onclick = \"alert('TUUT!')\">EN TUU</button></td></tr>" 
-                    # print(str(paiva) + " >> " + tyoviikko[i] + " >> " + aihe)
-                # else:
-                #     print(str(paiva) + " >> " + tyoviikko[i] + " >> " + "---")
+                    lukuhtml = lukuhtml + "<tr><td>" + str(paiva) + "." + str(kuu) + "</td><td>" + tyoviikko[i] + "</td><td>" + aihe + "</td><td><button type = 'button' \
+                    onclick = \"alert('OK " + name.title() +  ", et tuu " + str(paiva) + "." + str(kuu) + ".'), poissa(" + str(paiva) +", " + str(kuu) + ")\">EN TUU</button></td></tr>"
             i = i + 1
 
     lukuhtml = Markup(lukuhtml + "</table>")
@@ -43,12 +43,13 @@ def app_name(name):
     verkkohtml = "<table><tr><th> # </th><th>Nimi</th><th>Aloittamatta</th><th>Kesken</th><th>Valmis</th></tr>"
     i = 0
     for kurssi in verkkokurssit:
-        verkkohtml = verkkohtml + "<tr><td>" + str(i+1) + "</td><td>" + kurssi + "</td><td>" + "<td><input type=\"radio\" name=\"verkkovaihe\" value=\"aloittamatta\"></td><td><input type=\"radio\" name=\"verkkovaihe\" value=\"kesken\"></td><td><input type=\"radio\" name=\"verkkovaihe\" value=\"valmis\"></td></tr>"
+        verkkohtml = verkkohtml + "<tr><td>" + str(i+1) + "</td><td>" + kurssi + "</td><td>" + \
+        "<td><input type=\"radio\" name=\"verkkovaihe\" value=\"aloittamatta\" checked></td><td><input type=\"radio\" name=\"verkkovaihe\" value=\"kesken\"></td><td><input type=\"radio\" name=\"verkkovaihe\" value=\"valmis\"></td></tr>"
         i = i + 1
 
-    verkkohtml = Markup(verkkohtml + "</table><br><button type = \"button\" onclick = \"alert('Tee enemmän!!')\">Submit</button>")
+    verkkohtml = Markup(verkkohtml + "</table><br><button type = \"button\" onclick = \"alert('OK, " + name.title() + "')\">Submit</button>")
 
-    return render_template('cal.html', name = name.title(), kuukauded=kuukauded[kuu-1], lukuhtml=lukuhtml, verkkohtml=verkkohtml)
+    return render_template('main.html', name = name.title(), kuukauded=kuukauded[kuu-1], lukuhtml=lukuhtml, verkkohtml=verkkohtml)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
